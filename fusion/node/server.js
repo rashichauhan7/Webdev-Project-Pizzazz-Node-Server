d329c20579fca9080b122ec647fdd5df14aba87b
rashichauhan7 <rashi.chauhan95@gmail.com>
2018-08-13 03:23:58 -0400

    added id specific search and autocomplete

--- fusion/node/server.js
+++ fusion/node/server.js
@@ -1,3 +1,5 @@
+var data = require('./categories.json')
+
 var express = require('express')
 var app = express()
 const yelp = require('yelp-fusion');
@@ -11,7 +13,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+const client = yelp.client(apiKey);
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,7 +26,7 @@ app.post('/search', function (req, res) {
         location: location,
         categories: 'hair, othersalons'
     };
-    const client = yelp.client(apiKey);
+
 
     client.search(searchRequest).then(response => {
         const Result = response.jsonBody.businesses;
@@ -43,4 +45,32 @@ app.post('/search', function (req, res) {
 
 })
 
-app.listen(4000)
+app.get('/business/:id', function (req, res) {
+     client.business(req.params['id'])
+         .then(response => res.send(response.jsonBody));
+
+})
+
+app.post('/autocomplete', function (req, res) {
+    const text = req.body.text;
+   parseResults(text , res);
+
+})
+
+function parseResults(search, res)
+{
+    let results = [];
+
+        data.map(function (category) {
+            if(category.parents.length !== 0 && (category.parents[0]).localeCompare("beautysvc") === 0)
+            {
+                if(category.title.toString().toLowerCase().includes(search.toString().toLowerCase()))
+                    results = [...results, category];
+            }
+
+        })
+
+    res.send(results);
+
+}
+app.listen(2000)
